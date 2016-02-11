21bcc56f0cd3df0b3ffc273905d83adc591f57ee
ozihler <ozihler@ozihler-ThinkPad-T410>
2016-02-11 12:30:43 +0100

    ddd

--- TomP2PTrials/src/test/java/mapreduce/engine/componenttests/SystemInteractionTest.java
+++ TomP2PTrials/src/test/java/mapreduce/engine/componenttests/SystemInteractionTest.java
@@ -58,12 +58,8 @@ public class SystemInteractionTest {
 
 	@Test
 	public void test() throws Exception {
-		int nrOfFiles = 1;
-<<<<<<< HEAD
-		int nrOfWords = 100;
-=======
-		int nrOfWords = 10;
->>>>>>> branch 'master' of https://github.com/ollyblink/mt2.git
+		int nrOfFiles = 1; 
+		int nrOfWords = 10; 
 		// String jsMapper = FileUtils.INSTANCE.readLines(System.getProperty("user.dir") + "/src/main/java/mapreduce/execution/procedures/wordcountmapper.js");
 		// System.out.println(jsMapper);
 		// String jsReducer = FileUtils.INSTANCE.readLines(System.getProperty("user.dir") + "/src/main/java/mapreduce/execution/procedures/wordcountreducer.js");
