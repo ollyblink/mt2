3b04283cf12877761b742365017270567c4132cf
Thomas Bocek <tom@tomp2p.net>
2016-02-24 15:29:20 +0100

    new mapreduce structure

---
+++ TomP2PTrials/src/main/java/net/tomp2p/mapreduce/Job.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.tomp2p.mapreduce;
+
+import java.util.List;
+import mapreduce.execution.procedures.Procedure;
+
+/**
+ *
+ * @author draft
+ */
+public class Job {
+    private List<Task> tasks;
+    //addTask
+    //jobId
+    //objcetDatareply
+}
