a6c9dd4cb214fe6ea378d6455b645bb19c73109b
ozihler <oliver_zihler@hotmail.com>
2016-02-27 10:24:42 +0100

    Bootstrapper

---
+++ TomP2PTrials/src/main/java/net/tomp2p/mapreduce/Main2.java
@@ -0,0 +1,25 @@
+package net.tomp2p.mapreduce;
+
+import mapreduce.storage.DHTConnectionProvider;
+
+public class Main2 {
+	public static void main(String[] args) {
+
+		DHTConnectionProvider dht = DHTConnectionProvider.create("192.168.43.65", 4000, 4000);
+		MapReduceBroadcastHandler broadcastHandler = new MapReduceBroadcastHandler(dht);
+		dht.broadcastHandler(broadcastHandler);
+		try {
+			dht.connect();
+
+			while (!dht.peerDHT().peer().isShutdown()) {
+				System.out.println("Sleeping 1s til shutdown");
+				Thread.sleep(1000);
+			}
+		} catch (Exception e) {
+			dht.shutdown();
+		} finally {
+			dht.shutdown();
+		}
+	}
+
+}
