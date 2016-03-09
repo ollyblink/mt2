2b185e821298c92ae5984634a3e360078daec777
ozihler <ozihler@ozihler-ThinkPad-T410>
2016-03-09 09:13:36 +0100

    changed package

--- TomP2PTrials/src/main/java/net/tomp2p/mapreduce/MainBootstrapperAndWorker.java
+++ TomP2PTrials/src/main/java/net/tomp2p/mapreduce/MainBootstrapperAndWorker.java
@@ -1,19 +1,17 @@
-package net.tomp2p.mapreduce.examplejob;
+package net.tomp2p.mapreduce;
 
 import java.net.InetAddress;
 import java.util.Random;
 
 import net.tomp2p.futures.BaseFutureAdapter;
 import net.tomp2p.futures.FutureBootstrap;
-import net.tomp2p.mapreduce.MapReduceBroadcastHandler;
-import net.tomp2p.mapreduce.PeerMapReduce;
 import net.tomp2p.p2p.Peer;
 import net.tomp2p.p2p.PeerBuilder;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.peers.PeerMap;
 import net.tomp2p.peers.PeerMapConfiguration;
 
-public class NonBootStrapper {
+public class MainBootstrapperAndWorker {
 	public static void main(String[] args) throws Exception {
 		String bootstrapperToConnectTo = "192.168.1.171";
 		int bootstrapperPortToConnectTo = 4004;
