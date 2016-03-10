93ec2e727d6af5c289cef2f3208098dc82e6fccd
ozihler <ozihler@ozihler-ThinkPad-T410>
2016-03-10 09:27:56 +0100

    vvvv

--- TomP2PTrials/src/main/java/net/tomp2p/mapreduce/MapReduceBroadcastHandler.java
+++ TomP2PTrials/src/main/java/net/tomp2p/mapreduce/MapReduceBroadcastHandler.java
@@ -37,70 +37,71 @@ public class MapReduceBroadcastHandler extends StructuredBroadcastHandler {
 	private PeerMapReduce peerMapReduce;
 
 	public MapReduceBroadcastHandler() {
-		int threads = Runtime.getRuntime().availableProcessors();
+//		int threads = Runtime.getRuntime().availableProcessors();
+		int threads = 1;
 		this.executor = new ThreadPoolExecutor(threads, threads, Long.MAX_VALUE, TimeUnit.DAYS, new LinkedBlockingQueue<>());
 	}
 
 	@Override
 	public StructuredBroadcastHandler receive(Message message) {
- //		if (!executor.isShutdown()) {
-//			executor.execute(new Runnable() {
-//
-//				@Override
-//				public void run() {
-//					try {
-//						NavigableMap<Number640, Data> input = message.dataMapList().get(0).dataMap();
-//						// inform peerConnectionActiveFlagRemoveListeners about completed/finished data processing newInput.put(NumberUtils.SENDER, new Data(pmr.peer().peerAddress()));
-//						if (input.containsKey(NumberUtils.SENDER) && input.containsKey(NumberUtils.INPUT_STORAGE_KEY)) { // Skips in first execution where there is no input
-//							PeerAddress peerAddress = (PeerAddress) input.get(NumberUtils.SENDER).object();
-////							logger.info("Received input storage key: [" + input.get(NumberUtils.INPUT_STORAGE_KEY).object() + "]");
-//							Number640 storageKey = (Number640) input.get(NumberUtils.INPUT_STORAGE_KEY).object();
-//							// if (!peerAddress.equals(peerMapReduce.peer().peerAddress())) {
-//							informPeerConnectionActiveFlagRemoveListeners(peerAddress, storageKey);
-//							// } else {
-//							// logger.info("Received message from myself I[" + peerMapReduce.peer().peerID().shortValue() + "]/Rec[" + peerAddress.peerId().shortValue() + "]... Ignores flag remove listener");
-//							// }
-//						}
-//						// Receivers need to be generated and added if they did not exist yet
-//						if (input.containsKey(NumberUtils.RECEIVERS)) {
-//							instantiateReceivers(((List<TransferObject>) input.get(NumberUtils.RECEIVERS).object()));
-//						}
-//						// Call receivers with new input data...
-//						// if (message.sender() != null) {
-//						passMessageToBroadcastReceivers(message);
-//						// }
-//
-//					} catch (Exception e) {
-//						logger.info("Exception caught", e);
-//					}
-//				}
-//			});
-//		}
-		try {
-			NavigableMap<Number640, Data> input = message.dataMapList().get(0).dataMap();
-			// inform peerConnectionActiveFlagRemoveListeners about completed/finished data processing newInput.put(NumberUtils.SENDER, new Data(pmr.peer().peerAddress()));
-			if (input.containsKey(NumberUtils.SENDER) && input.containsKey(NumberUtils.INPUT_STORAGE_KEY)) { // Skips in first execution where there is no input
-				PeerAddress peerAddress = (PeerAddress) input.get(NumberUtils.SENDER).object();
-				logger.info("Received input storage key: [" + input.get(NumberUtils.INPUT_STORAGE_KEY).object() + "]");
-				Number640 storageKey = (Number640) input.get(NumberUtils.INPUT_STORAGE_KEY).object();
-				// if (!peerAddress.equals(peerMapReduce.peer().peerAddress())) {
-				informPeerConnectionActiveFlagRemoveListeners(peerAddress, storageKey);
-				// } else {
-				// logger.info("Received message from myself I[" + peerMapReduce.peer().peerID().shortValue() + "]/Rec[" + peerAddress.peerId().shortValue() + "]... Ignores flag remove listener");
-				// }
-			}
-			// Receivers need to be generated and added if they did not exist yet
-			if (input.containsKey(NumberUtils.RECEIVERS)) {
-				instantiateReceivers(((List<TransferObject>) input.get(NumberUtils.RECEIVERS).object()));
-			}
-			// Call receivers with new input data...
-			// if (message.sender() != null) {
-			passMessageToBroadcastReceivers(message);
-			// }
+ 		if (!executor.isShutdown()) {
+			executor.execute(new Runnable() {
 
-		} catch (Exception e) {
-			logger.info("Exception caught", e);
+				@Override
+				public void run() {
+					try {
+						NavigableMap<Number640, Data> input = message.dataMapList().get(0).dataMap();
+						// inform peerConnectionActiveFlagRemoveListeners about completed/finished data processing newInput.put(NumberUtils.SENDER, new Data(pmr.peer().peerAddress()));
+						if (input.containsKey(NumberUtils.SENDER) && input.containsKey(NumberUtils.INPUT_STORAGE_KEY)) { // Skips in first execution where there is no input
+							PeerAddress peerAddress = (PeerAddress) input.get(NumberUtils.SENDER).object();
+//							logger.info("Received input storage key: [" + input.get(NumberUtils.INPUT_STORAGE_KEY).object() + "]");
+							Number640 storageKey = (Number640) input.get(NumberUtils.INPUT_STORAGE_KEY).object();
+							// if (!peerAddress.equals(peerMapReduce.peer().peerAddress())) {
+							informPeerConnectionActiveFlagRemoveListeners(peerAddress, storageKey);
+							// } else {
+							// logger.info("Received message from myself I[" + peerMapReduce.peer().peerID().shortValue() + "]/Rec[" + peerAddress.peerId().shortValue() + "]... Ignores flag remove listener");
+							// }
+						}
+						// Receivers need to be generated and added if they did not exist yet
+						if (input.containsKey(NumberUtils.RECEIVERS)) {
+							instantiateReceivers(((List<TransferObject>) input.get(NumberUtils.RECEIVERS).object()));
+						}
+						// Call receivers with new input data...
+						// if (message.sender() != null) {
+						passMessageToBroadcastReceivers(message);
+						// }
+
+					} catch (Exception e) {
+						logger.info("Exception caught", e);
+					}
+				}
+			});
 		}
+//		try {
+//			NavigableMap<Number640, Data> input = message.dataMapList().get(0).dataMap();
+//			// inform peerConnectionActiveFlagRemoveListeners about completed/finished data processing newInput.put(NumberUtils.SENDER, new Data(pmr.peer().peerAddress()));
+//			if (input.containsKey(NumberUtils.SENDER) && input.containsKey(NumberUtils.INPUT_STORAGE_KEY)) { // Skips in first execution where there is no input
+//				PeerAddress peerAddress = (PeerAddress) input.get(NumberUtils.SENDER).object();
+//				logger.info("Received input storage key: [" + input.get(NumberUtils.INPUT_STORAGE_KEY).object() + "]");
+//				Number640 storageKey = (Number640) input.get(NumberUtils.INPUT_STORAGE_KEY).object();
+//				// if (!peerAddress.equals(peerMapReduce.peer().peerAddress())) {
+//				informPeerConnectionActiveFlagRemoveListeners(peerAddress, storageKey);
+//				// } else {
+//				// logger.info("Received message from myself I[" + peerMapReduce.peer().peerID().shortValue() + "]/Rec[" + peerAddress.peerId().shortValue() + "]... Ignores flag remove listener");
+//				// }
+//			}
+//			// Receivers need to be generated and added if they did not exist yet
+//			if (input.containsKey(NumberUtils.RECEIVERS)) {
+//				instantiateReceivers(((List<TransferObject>) input.get(NumberUtils.RECEIVERS).object()));
+//			}
+//			// Call receivers with new input data...
+//			// if (message.sender() != null) {
+//			passMessageToBroadcastReceivers(message);
+//			// }
+//
+//		} catch (Exception e) {
+//			logger.info("Exception caught", e);
+//		}
 		return super.receive(message);
 	}
 
