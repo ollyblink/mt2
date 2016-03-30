f7ddbfa7533ffd5eb00d09d486b6a4c707bcac49
ozihler <ozihler@ozihler-ThinkPad-T410>
2016-03-30 12:08:19 +0200

    dddpeermapreduce

--- TomP2PTrials/src/main/java/net/tomp2p/mapreduce/PeerMapReduce.java
+++ TomP2PTrials/src/main/java/net/tomp2p/mapreduce/PeerMapReduce.java
@@ -70,6 +70,12 @@ public class PeerMapReduce {
 	}
 
 	public MapReduceGetBuilder get(Number160 locationKey, Number160 domainKey, NavigableMap<Number640, Data> broadcastInput) {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return new MapReduceGetBuilder(this, locationKey, domainKey).broadcastInput(broadcastInput);
 	}
 
