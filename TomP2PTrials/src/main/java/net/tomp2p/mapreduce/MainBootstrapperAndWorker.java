9a5102edb0031a015fc69a0e26713b9bf1be02e5
ozihler <ozihler@ozihler-ThinkPad-T410>
2016-03-31 08:27:18 +0200

    qewrqwer

--- TomP2PTrials/src/main/java/net/tomp2p/mapreduce/MainBootstrapperAndWorker.java
+++ TomP2PTrials/src/main/java/net/tomp2p/mapreduce/MainBootstrapperAndWorker.java
@@ -27,7 +27,7 @@ public class MainBootstrapperAndWorker {
 		// int nrOfFiles = 5;
 		PeerConnectionCloseListener.WAITING_TIME = Integer.MAX_VALUE; // Should be less than shutdown time (reps*sleepingTime)
 		//
-		String bootstrapperToConnectTo = "192.168.0.12";
+		String bootstrapperToConnectTo = "130.60.156.102";
 		int bootstrapperPortToConnectTo = 4004;
 		// MapReduceBroadcastHandler broadcastHandler = new MapReduceBroadcastHandler();
 
